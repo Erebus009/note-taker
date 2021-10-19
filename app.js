d0d967d9b9b7d1680a2b9163c7d946236d3aad25
Travis Puryear <travispuryear678@gmail.com>
2021-10-19 17:37:52 -0400

    First intial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const { json } = require('express');
+const express = require('express');
+const fs = require('fs');
+const path = require('path')
+// PORT server will open ti display
+const PORT = 3000;
+// Calls the express function to a const to be used in Methods (post,get,fetch...etc...etc)
+const app = express();
+// handles data parsing.
+app.use(express.urlencoded({extended:true}));
+app.use(express.json())
+
+// Set up the page to use public folder assets from one path file instead of naming all the paths one by one.
+app.use(express.static('public'));
+
+
+// GET method to display notes.html when get started button is clicked and wants to request http://localhost:3000/notes
+app.get('/notes',(req,res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+
+})
+
+
+
+
+
+
+
+// listens to port and logs out for easy access with ctrl+click.
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}`);
+})

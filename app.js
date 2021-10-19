c10cb0f8be3a809199d1a760884b279849650bd2
Travis Puryear <travispuryear678@gmail.com>
2021-10-19 18:01:55 -0400

    First pathing setup for index and notes using public static folder files.

--- app.js
+++ app.js
@@ -13,13 +13,20 @@ app.use(express.json())
 // Set up the page to use public folder assets from one path file instead of naming all the paths one by one.
 app.use(express.static('public'));
 
+app.get('/',(req,res) =>{
+    res.sendFile(path.join(__dirname, '/public/'))
+})
+
 
 // GET method to display notes.html when get started button is clicked and wants to request http://localhost:3000/notes
 app.get('/notes',(req,res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 
 })
-
+// GEt mothod to display notes database API
+app.get('/api/notes', (req,res) =>{
+    res.sendFile(path.join(__dirname, './db/db.json'))
+})
 
 
 
